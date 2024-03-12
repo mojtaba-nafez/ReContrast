5d79c5e0e1ec2a907ff57b6cd2bcd827005f204e
Moein Madadi <moeinmadadi2002@gmail.com>
2024-03-13 01:46:55 +0330

    testing

--- recontrast_mvtec.py
+++ recontrast_mvtec.py
@@ -82,7 +82,7 @@ def visualize_random_samples_from_clean_dataset(dataset, dataset_name):
     random_samples = [dataset[i] for i in random_indices]
 
     # Separate images and labels
-    images, labels, *rest = zip(*random_samples)
+    images, labels = zip(*random_samples)
 
     print(f"len(labels): {len(labels)}")
     print(f"type(labels): {type(labels)}")

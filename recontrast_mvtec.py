2f92f32b3708431fdbe58079b2c4c24d9ee3efcc
Arad Maleki <arad.m.t02@gmail.com>
2024-04-21 18:01:11 +0330

    update

--- recontrast_mvtec.py
+++ recontrast_mvtec.py
@@ -247,7 +247,7 @@ def train(_class_, shrink_factor=None, total_iters=2000, evaluation_epochs=250, 
             cls_output = cls(en_3rd)
             print('en3', en_3rd.shape, en_3rd.dtype)
             print('anomaly_data', anomaly_data.shape, anomaly_data.dtype, anomaly_data.long().dtype, cls_output.dtype)
-            cls_loss = criterion(cls_output, anomaly_data)
+            cls_loss = criterion(cls_output, anomaly_data.to(torch.int64))
 
             print('lolz')
             alpha_final = 1

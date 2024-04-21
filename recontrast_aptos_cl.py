a21e4a218691a7197fcd7e6ffcfeceab1bf22e95
mojtaba nafez <mojtabanafez96@gmail.com>
2024-04-22 02:48:36 +0330

    update

--- recontrast_aptos_cl.py
+++ recontrast_aptos_cl.py
@@ -232,13 +232,13 @@ def train(_class_, shrink_factor=None, total_iters=2000, evaluation_epochs=250, 
 
                 shrink_factor = "main" 
                 # auroc, f1, acc = evaluation_noseg(model, test_dataloader1, device)
-                auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)] = evaluation_noseg(model, test_dataloader1, device)
+                auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)] = evaluation_noseg_brain(model, test_dataloader1, device)
                 print_fn('Shrink Factor:{}, Sample Auroc:{:.3f}, F1:{:.3f}, acc:{:.3}'.format(shrink_factor, auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)]))
                 if auroc_sp_list[str(shrink_factor)] >= auroc_sp_list_best[str(shrink_factor)]:
                     auroc_px_list_best[str(shrink_factor)], auroc_sp_list_best[str(shrink_factor)], auroc_aupro_px_list_best[str(shrink_factor)] = auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)]
                 
                 shrink_factor = "shifted"
-                auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)] = evaluation_noseg(model, test_dataloader2, device)
+                auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)] = evaluation_noseg_brain(model, test_dataloader2, device)
                 print_fn('Shrink Factor:{}, Sample Auroc:{:.3f}, F1:{:.3f}, acc:{:.3}'.format(shrink_factor, auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)]))
                 if auroc_sp_list[str(shrink_factor)] >= auroc_sp_list_best[str(shrink_factor)]:
                     auroc_px_list_best[str(shrink_factor)], auroc_sp_list_best[str(shrink_factor)], auroc_aupro_px_list_best[str(shrink_factor)] = auroc_px_list[str(shrink_factor)], auroc_sp_list[str(shrink_factor)], auroc_aupro_px_list[str(shrink_factor)]

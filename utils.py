5d4fbadec351932275a47c24d83323584cedeeda
Arad Maleki <arad.m.t02@gmail.com>
2024-04-23 18:07:31 +0330

    debug

--- utils.py
+++ utils.py
@@ -264,7 +264,6 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
             cls_list_sp_normal = []
             cls_list_sp_anomaly = []
             for img, label in train_loader:
-                print('shape', img.shape, label.shape)
                 img = img.to(device)
                 # -------------------normal--------------------------
                 if not head_end:
@@ -307,7 +306,7 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
 
             w_map[0] = 1 / (np.sum(pr_list_sp_normal) / len(pr_list_sp_normal))
             # w_map[1] = 1 / (np.sum(pr_list_sp_anomaly) / len(pr_list_sp_anomaly))
-            print('cls list: ', cls_list_sp_anomaly)
+            print('cls list: ', cls_list_sp_normal)
             w_msp[0] = 1 / (np.sum(cls_list_sp_normal) / len(cls_list_sp_normal))
             # w_msp[1] = 1 / (np.sum(cls_list_sp_anomaly) / len(cls_list_sp_anomaly))
             print(f'weight of max map score (normal): {w_map[0]}')

68501733f9f2d9a2f7d29e205b346c3f11e9f60c
Arad Maleki <arad.m.t02@gmail.com>
2024-04-23 16:04:48 +0330

    debug

--- recontrast_isic_cl.py
+++ recontrast_isic_cl.py
@@ -186,14 +186,14 @@ def global_cosine_hm_1(a, b, anomaly_data, alpha=1., factor=0.):
 
 
 class BinaryClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=1024):
         super(BinaryClassifier, self).__init__()
         # input shape: [Batch size, 256, 16, 16]
         self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
         # output shape: [Batch size, 256, 1, 1]
         self.flatten = nn.Flatten()
         # output shape: [Batch size, 256]
-        self.fc = nn.Linear(256, 2)
+        self.fc = nn.Linear(in_channels, 2)
 
     def forward(self, x):
         x = self.adaptive_pool(x)
@@ -257,17 +257,19 @@ def train(_class_, shrink_factor=None, total_iters=2000, evaluation_epochs=250, 
     # visualize_random_samples_from_clean_dataset(train_data, f"train_data_{_class_}", train_data=True)
     # visualize_random_samples_from_clean_dataset(test_data, f"test_data_{_class_}", train_data=False)
 
+    in_channels = 1024
     if model == 'wide_res50':
         encoder, bn = wide_resnet50_2(pretrained=True)
         decoder = de_wide_resnet50_2(pretrained=False, output_conv=2)
     elif model == 'res18':
         encoder, bn = resnet18(pretrained=True)
         decoder = de_resnet18(pretrained=False, output_conv=2)
+        in_channels = 256
     else:
         encoder, bn = wide_resnet50_2(pretrained=True)
         decoder = de_wide_resnet50_2(pretrained=False, output_conv=2)
     if not head_end:
-        cls = BinaryClassifier()
+        cls = BinaryClassifier(in_channels=in_channels)
     else:
         cls = BinaryClassifier2()
     cls = cls.to(device)

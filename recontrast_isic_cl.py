d0e96626c0d35c3b648231016eb866aaac8c7e8c
Arad Maleki <arad.m.t02@gmail.com>
2024-04-23 21:16:50 +0330

    debug

--- recontrast_isic_cl.py
+++ recontrast_isic_cl.py
@@ -219,8 +219,8 @@ def train(_class_, shrink_factor=None, total_iters=2000, evaluation_epochs=250, 
     setup_seed(111)
 
     total_iters = total_iters
-    image_size = 256
-    crop_size = 256
+    image_size = image_size
+    crop_size = image_size
 
     if augmented_view:
         train_data_transforms = transforms.Compose([

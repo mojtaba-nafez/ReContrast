912bc007d51779d92584c75a1570db98dd375ba2
Arad Maleki <arad.m.t02@gmail.com>
2024-04-23 18:02:46 +0330

    debug

--- utils.py
+++ utils.py
@@ -306,6 +306,7 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
 
             w_map[0] = 1 / (np.sum(pr_list_sp_normal) / len(pr_list_sp_normal))
             # w_map[1] = 1 / (np.sum(pr_list_sp_anomaly) / len(pr_list_sp_anomaly))
+            print('cls list: ', cls_list_sp_anomaly)
             w_msp[0] = 1 / (np.sum(cls_list_sp_normal) / len(cls_list_sp_normal))
             # w_msp[1] = 1 / (np.sum(cls_list_sp_anomaly) / len(cls_list_sp_anomaly))
             print(f'weight of max map score (normal): {w_map[0]}')

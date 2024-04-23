ff9ae28c53159d8a32019816098c7088ab1d55f6
Mahdi-Marv <mahdihajialilue@gmail.com>
2024-04-23 21:23:04 +0330

    fix bug

--- models/resnet.py
+++ models/resnet.py
@@ -189,8 +189,8 @@ class ResNet(nn.Module):
 
         # print('printing layer 4\n', self.layer4)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        if fc:
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         mu = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1).cuda()
         std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1).cuda()

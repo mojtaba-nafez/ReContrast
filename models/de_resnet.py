f6c2a55ad192225bb621062d583190df60d5646d
mojtaba nafez <mojtabanafez96@gmail.com>
2024-04-27 12:49:39 +0330

    update

--- models/de_resnet.py
+++ models/de_resnet.py
@@ -327,7 +327,8 @@ def _resnet(
     if pretrained:
         if decoder_path is not None:
             dic = torch.load(decoder_path)
-            model.load_state_dict(dic, strict=False)
+            model.load_state_dict(dic)
+            print('decoder loaded')
         else:
             state_dict = load_state_dict_from_url(model_urls[arch],
                                                   progress=progress)

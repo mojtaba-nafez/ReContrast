57c0fca88ed6405480a07bee96bf558348bd31db
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 20:58:56 +0330

    debug

--- models/resnet.py
+++ models/resnet.py
@@ -203,6 +203,7 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.pretrain_unode_weghts = pretrain_unode_weghts
         end_size = 2048 if is_wide else 512
 
         if not unode:
@@ -285,7 +286,8 @@ class ResNet(nn.Module):
             feature_d = self.layer4(feature_c)
             feature_d = self.avgpool(feature_d)
             feature_d = torch.flatten(feature_d, 1)
-            feature_d = F.normalize(feature_d, dim=1)
+            if self.pretrain_unode_weghts:
+                feature_d = F.normalize(feature_d, dim=1)
             return self.shift_cls_layer(feature_d)
         
         return [feature_a, feature_b, feature_c]
@@ -309,7 +311,8 @@ class ResNet(nn.Module):
         if eval_unode:
             feature_d = self.avgpool(feature_d)
             feature_d = torch.flatten(feature_d, 1)
-            feature_d = F.normalize(feature_d, dim=1)
+            if self.pretrain_unode_weghts:
+                feature_d = F.normalize(feature_d, dim=1)
             return self.shift_cls_layer(feature_d)
         # print('en out:', feature_a.shape, feature_b.shape, feature_c.shape)
         return [feature_a, feature_b, feature_c, feature_d]

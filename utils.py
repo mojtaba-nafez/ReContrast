1a2c30c7cfdf613ad8de626840413b57935ef373
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 20:08:56 +0330

    debug

--- utils.py
+++ utils.py
@@ -352,7 +352,7 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
                 pr_list_sp.append(w_map * np.mean(anomaly_map))
 
             simclr_aug = simclr_aug.to(device)
-            img = simclr_aug(img)
+            # img = simclr_aug(img)
 
             unode_cls = model(img, eval_unode=True)
             unode_cls_score = w_unode * unode_cls[:, 0] * -1

70b4d843179a4263af3b65652c89adf5e2a446f6
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 11:23:28 +0330

    debug

--- utils.py
+++ utils.py
@@ -288,8 +288,6 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
                     pr_list_sp_normal.append(np.mean(anomaly_map))
 
                 unode_cls = model(img, eval_unode=True)
-                unode_cls = model(img, eval_unode=True)
-                unode_cls = model(img, eval_unode=True)
                 unode_cls_score = unode_cls[:, 0]
                 cls_list_unode_normal.append(unode_cls_score.cpu().numpy()[0])
                 print(f'counter: {counter}, unode cls score: {cls_list_unode_normal[-1]}')
@@ -333,7 +331,7 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
                 pr_list_sp.append(w_map * np.mean(anomaly_map))
 
             unode_cls = model(img, eval_unode=True)
-            unode_cls_score = w_unode * unode_cls[:, 0]
+            unode_cls_score = w_unode * unode_cls[:, 0] * -1
             unode_cls_list_sp.append(unode_cls_score.cpu().numpy()[0])
 
         thresh = return_best_thr(gt_list_sp, pr_list_sp)

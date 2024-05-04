e320d81beacc86eb7f1fe7929fece41690a7db60
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 13:19:00 +0330

    debug

--- recontrast_mvtec_cl.py
+++ recontrast_mvtec_cl.py
@@ -331,7 +331,7 @@ def train(_class_, shrink_factor=None, total_iters=2000, evaluation_epochs=250, 
     ######### from UNode
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        # transforms.CenterCrop((image_size, image_size)),
+        transforms.CenterCrop((image_size, image_size)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ])

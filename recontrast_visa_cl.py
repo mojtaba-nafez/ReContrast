fde8ef6869d7b1c3ad64abd5ec8e18f687fb5cc9
mojtaba nafez <mojtabanafez96@gmail.com>
2024-05-09 15:37:20 +0330

    update

--- recontrast_visa_cl.py
+++ recontrast_visa_cl.py
@@ -504,7 +504,7 @@ if __name__ == '__main__':
     pad_size = ["main", "shifted"]
     ii = 0
     for item in item_list:
-        unode_path = f'{ii}.model'
+        unode_path = f'./{ii}.pth'
         print(f"+++++++++++++++++++++++++++++++++++++++{item}+++++++++++++++++++++++++++++++++++++++")
         auroc_px, auroc_sp, aupro_px, auroc_sp_cls, auroc_px_best, auroc_sp_best, aupro_px_best, auroc_sp_cls_best = train(
             item,

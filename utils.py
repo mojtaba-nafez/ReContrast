03fded391a5d4e3d90cddf1aba98e28c57c7f415
Arad Maleki <arad.m.t02@gmail.com>
2024-05-10 18:22:27 +0330

    debug

--- utils.py
+++ utils.py
@@ -328,7 +328,8 @@ def evaluation_noseg_brain(model, dataloader, device, _class_=None, reduction='m
                     set_random_seed(seed)
                     img_temp = simclr_aug(img)
                     unode_cls = model(img_temp, eval_unode=eval_unode)
-                    print('!!!', len(unode_cls), len(unode_cls[0]))
+                    print('!!!', [unode_cls[0][i].shape for i in range(6)])
+                    print('!!!', [unode_cls[1][i].shape for i in range(6)])
                     unode_cls_score = unode_cls[:, 0]
                     seed_unode_cls.append(list(unode_cls_score.cpu().numpy()))
                 cls_list_unode_normal.extend(np.mean(np.array(seed_unode_cls), axis=0))

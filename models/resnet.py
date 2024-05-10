91e8c4a7a7035c358899c7256058eae76cd6c8f1
mojtaba nafez <mojtabanafez96@gmail.com>
2024-05-10 19:42:25 +0330

    update

--- models/resnet.py
+++ models/resnet.py
@@ -337,6 +337,7 @@ def _resnet(
     #    model.layer4 = None
     #    model.fc = None
     if pretrained:
+        unode= False
         if unode:
             dic = torch.load(unode_path)
             # print('loaded keys:', dic.keys())
